aeca66c0637626058fce45e2d9a971739ac0eee9
DhanashreeNavarkar <navarkardhanashree@gmail.com>
2020-09-30 16:54:43 +0530

    demo

--- src/co/webdriver/basics/javascriptexecutor/JavaScriptExecutorConcept.java
+++ src/co/webdriver/basics/javascriptexecutor/JavaScriptExecutorConcept.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
